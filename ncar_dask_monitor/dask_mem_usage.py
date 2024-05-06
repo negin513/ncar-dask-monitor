3e07c3ced14b72be8622626573081014a75af265
Negin Sobhani <negin513@gmail.com>
2024-05-06 11:28:08 -0600

    Update dask_mem_usage.py

--- ncar_dask_monitor/dask_mem_usage.py
+++ ncar_dask_monitor/dask_mem_usage.py
@@ -99,7 +99,7 @@ def get_parser():
         dest="table",
         required=False,
         action="store_true",
-        help="Write user report in a table format. [default: %(default)s]",
+        help="Only write a summary of user report in a table format. [default: %(default)s]",
     )
 
     parser.add_argument(
